4ae1a6d71f4e57433605768024e05437c92cf0bc
Juan Manuel VM <juan.vargas.munoz@correounivalle.edu.co>
2025-07-18 19:15:43 -0500

    Adicionar Preguntas

--- SolucionArbol/solucion1.py
+++ SolucionArbol/solucion1.py
@@ -18,7 +18,29 @@ class Encuestado:
             return self.experticia > other.experticia
         return self.id > other.id
         
+class Pregunta:
+    def __init__(self, id):
+        self.id = id
+        self.encuestados = ArbolRB()
 
+    def __str__(self):
+        return f"(Id: {self.id})"
+    
+    def promedio_opinion(self):
+        return self.encuestados.obtener_promedio("opinion")
+
+    def promedio_experticia(self):
+        return self.encuestados.obtener_promedio("experticia")
+
+    def cantidad_encuestados(self):
+        return self.encuestados.contar_nodos()
+
+    def __gt__(self, other):
+        if self.promedio_opinion() != other.promedio_opinion():
+            return self.promedio_opinion() < other.promedio_opinion()
+        if self.promedio_experticia() != other.promedio_experticia():
+            return self.promedio_experticia() < other.promedio_experticia()
+        return self.cantidad_encuestados() < other.cantidad_encuestados()
     
 class NodoRB:
     def __init__(self, dato):
@@ -27,7 +49,7 @@ class NodoRB:
         self.izq = None
         self.der = None
         self.padre = None
-    
+
     def __str__(self):
         return self.dato
 
@@ -103,6 +125,32 @@ class ArbolRB:
         self._inorden_rec(nodo.izq, resultado)
         resultado.append(nodo.dato)
         self._inorden_rec(nodo.der, resultado)
+
+    def contar_nodos(self):
+        return self._contar(self.raiz)
+
+    def _contar(self, nodo):
+        if nodo is None:
+            return 0
+        return 1 + self._contar(nodo.izq) + self._contar(nodo.der)
+
+    def obtener_promedio(self, atributo):
+        suma, cantidad = self._sumar_y_contar(self.raiz, atributo)
+        return suma / cantidad if cantidad > 0 else 0
+
+    def _sumar_y_contar(self, nodo, atributo):
+        if nodo is None:
+            return 0, 0
+        if atributo == "opinion":
+            valor = nodo.dato.opinion
+        elif atributo == "experticia":
+            valor = nodo.dato.experticia
+        else:
+            valor = 0
+        suma_izq, cant_izq = self._sumar_y_contar(nodo.izq, atributo)
+        suma_der, cant_der = self._sumar_y_contar(nodo.der, atributo)
+        return valor + suma_izq + suma_der, 1 + cant_izq + cant_der
+
         
 if __name__ == "__main__":
     e1 = Encuestado(1, "elkin", 9, 6)
@@ -114,7 +162,27 @@ if __name__ == "__main__":
     e7 = Encuestado(8, "elkin", 1, 7)
     e8 = Encuestado(7, "elkin", 1, 7)
     arn = ArbolRB()
+
+    p1 = Pregunta(1)
+    p2 = Pregunta(2)
+    arnPreguntas = ArbolRB()
     print(e2>e1)
+
+    p1.encuestados.insertar(e1)
+    p1.encuestados.insertar(e2)
+    p1.encuestados.insertar(e3)
+    p1.encuestados.insertar(e4)
+    p2.encuestados.insertar(e5)
+    p2.encuestados.insertar(e6)
+    p2.encuestados.insertar(e7)
+    p2.encuestados.insertar(e8)
+    arnPreguntas.insertar(p1)
+    arnPreguntas.insertar(p2)
+    
+    for e in arnPreguntas.inorden():
+        print(e)
+    print(p1.encuestados.obtener_promedio("opinion"))
+    print(p2.encuestados.obtener_promedio("opinion"))
 
     arn.insertar(e1)
     arn.insertar(e2)
